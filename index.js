787afa115512f2c62d6143a1fe217758f0ab5cba
Marty Rabatel <marty.rabatel2@gmail.com>
2024-09-16 12:07:06 +0200

    IA console

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
-const fs = require("fs");
+import "dotenv/config";
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+import fs from "fs"; // Permet de lire/ecrire des fichiers
+
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+
+console.log("process.env.GOOGLE_API_KEY", process.env.GOOGLE_API_KEY);
 
 async function run() {
   const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-  const result = await model.generateContent([
-    "What is in this photo?",
-    {
-      inlineData: {
-        data: Buffer.from(fs.readFileSync("path/to/image.png")).toString(
-          "base64"
-        ),
-        mimeType: "image/png",
-      },
-    },
-  ]);
-  console.log(result.response.text());
+
+  const prompt = "BMW ou Audi ?";
+
+  const result = await model.generateContent(prompt);
+
+  console.log("voici le resultat: ", result.response.text());
 }
 run();
