f32c06aade3b3b88a1d895e7e88caac8cc00416e
Marty Rabatel <marty.rabatel2@gmail.com>
2024-09-17 11:20:59 +0200

    Ajout du chatbot fonctionnel

--- controller/chatbotController.js
+++ controller/chatbotController.js
@@ -1,13 +1,37 @@
+import "dotenv/config";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+import fs from "fs"; // Permet de lire/ecrire des fichiers
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+console.log("process.env.GOOGLE_API_KEY", process.env.GOOGLE_API_KEY);
+
 export const chatbot = (req, res) => {
   return res.render("index");
 };
 
-const search = (req, res) => {
+async function run(a) {
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+
+  const prompt = a;
+
+  const result = await model.generateContent(prompt);
+
+  console.log("voici le resultat: ", result.response.text());
+
+  const r = result;
+
+  return r;
+}
+
+const search = async (req, res) => {
   // Afficher en console les données POST reçues
   console.log("Données reçues en POST :", req.body);
 
+  const a = await run(req.body.message);
+  console.log(a.response.text());
+
   // Simuler des données à passer à la vue
-  const data = req.body;
+  const data = a.response.text();
 
   // Rendre la vue index.ejs avec des données simulées
   return res.render("index", { data });
