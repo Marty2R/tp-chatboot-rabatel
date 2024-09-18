c8a0b7f2a2726440da5e510e1249b99a1a846e68
Marty Rabatel <marty.rabatel2@gmail.com>
2024-09-18 13:47:01 +0200

    update

--- controller/chatbotController.js
+++ controller/chatbotController.js
@@ -6,7 +6,8 @@ const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
 console.log("process.env.GOOGLE_API_KEY", process.env.GOOGLE_API_KEY);
 
 export const chatbot = (req, res) => {
-  return res.render("index");
+  const conv = [];
+  return res.render("index", { conv });
 };
 
 async function run(a) {
@@ -25,18 +26,21 @@ async function run(a) {
 
 const search = async (req, res) => {
   // Afficher en console les données POST reçues
-  console.log("Données reçues en POST :", req.body);
+  //console.log("Données reçues en POST :", req.body);
 
   const a = await run(req.body.message);
-  console.log(a.response.text());
 
   // Simuler des données à passer à la vue
   const data = a.response.text();
 
+  const conv = [];
+
   const question = req.body.message;
 
+  conv.push({ question, reponse: data });
+
   // Rendre la vue index.ejs avec des données simulées
-  return res.render("index", { question, data });
+  return res.render("index", { conv });
 };
 
 export default { chatbot, search };
